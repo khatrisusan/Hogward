36909fa7df448a851083b595239cbfcb51fa44d2
Susan Khatri <khatrisusan>
2020-02-26 14:44:38 +0100

    did template and forEach

--- hogward.js
+++ hogward.js
@@ -1,1 +1,25 @@
 "use strict";
+window.addEventListener("DOMContentLoaded", init);
+function init() {
+  console.log("ready");
+  fetchStudentList();
+}
+function fetchStudentList() {
+  fetch(" https://petlatkea.dk/2020/hogwarts/students.json")
+    .then(function(result) {
+      return result.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      data.forEach(showStudent);
+    });
+}
+function showStudent(student) {
+  console.log(student);
+  let template = document.querySelector("template").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector(".fullName").textContent = student.fullname;
+  copy.querySelector(".gender").textContent = student.gender;
+  copy.querySelector(".house").textContent = student.house;
+  document.querySelector("main").appendChild(copy);
+}
